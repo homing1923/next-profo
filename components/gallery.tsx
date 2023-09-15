1c06cad99a33518358ae8df86e62a104f900d8f6
homing1923 <homing1923@gmail.com>
2023-09-14 22:55:43 -0400

    alot

---
+++ components/gallery.tsx
@@ -0,0 +1,16 @@
+import PhotoAlbum from "react-photo-album";
+import NextJsImage from "/components/NextJsImage";
+
+const Gallery = ({photos}) => (
+    <div>
+    <PhotoAlbum 
+    layout="rows" 
+    photos={photos} 
+    renderPhoto={NextJsImage}
+    defaultContainerWidth={1200}
+            sizes={{ size: "calc(100vw - 240px)" }}
+    />
+    </div>
+);
+
+export default Gallery;
